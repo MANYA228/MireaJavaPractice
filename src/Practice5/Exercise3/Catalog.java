889b3703739863d4f1393e47c87fd86a2bd33396
MANYA <71900183+MANYA228@users.noreply.github.com>
2023-10-15 19:12:17 +0300

    Practice 5 Ready !
    Signed-off-by: MANYA <71900183+MANYA228@users.noreply.github.com>

--- src/Practice5/Exercise3/Catalog.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-enum Catalog {
-    ELECTRONICS,
-    CLOTHING,
-    BOOKS
-}
-
---
+++ src/Practice5/Exercise3/Main.java
@@ -0,0 +1,125 @@
+package Practice5.Exercise3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+// Класс, представляющий товар
+class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+// Класс, представляющий каталог товаров
+class Catalog {
+    private String name;
+    private List<Product> products;
+
+    public Catalog(String name) {
+        this.name = name;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Класс, представляющий интернет-магазин
+Ь
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем интернет-магазин
+        OnlineStore store = new OnlineStore();
+
+        // Создаем каталоги товаров
+        Catalog electronicsCatalog = new Catalog("Электроника");
+        electronicsCatalog.addProduct(new Product("Смартфон", 599.99));
+        electronicsCatalog.addProduct(new Product("Ноутбук", 999.99));
+        electronicsCatalog.addProduct(new Product("Планшет", 299.99));
+
+        Catalog clothingCatalog = new Catalog("Одежда");
+        clothingCatalog.addProduct(new Product("Футболка", 19.99));
+        clothingCatalog.addProduct(new Product("Джинсы", 49.99));
+        clothingCatalog.addProduct(new Product("Куртка", 79.99));
+
+        store.addCatalog(electronicsCatalog);
+        store.addCatalog(clothingCatalog);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isAuthenticated = false;
+
+        // Реализация аутентификации пользователя (просто для примера)
+        while (!isAuthenticated) {
+            System.out.print("Введите логин: ");
+            String username = scanner.nextLine();
+            System.out.print("Введите пароль: ");
+            String password = scanner.nextLine();
+            // Здесь можно добавить проверку логина и пароля в базе данных пользователей
+            // В данном примере просто предполагается, что аутентификация успешна
+            isAuthenticated = true;
+            System.out.println("Вы успешно вошли в систему.");
+        }
+
+        while (true) {
+            System.out.println("\nВыберите действие:");
+            System.out.println("1. Просмотреть каталоги товаров");
+            System.out.println("2. Просмотреть товары в каталоге");
+            System.out.println("3. Добавить товар в корзину");
+            System.out.println("4. Оформить покупку");
+            System.out.println("0. Выйти из приложения");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Считываем символ новой строки
+
+            switch (choice) {
+                case 1:
+                    store.displayCatalogs();
+                    break;
+                case 2:
+                    System.out.print("Введите имя каталога: ");
+                    String catalogName = scanner.nextLine();
+                    store.displayProductsInCatalog(catalogName);
+                    break;
+                case 3:
+                    System.out.print("Введите имя товара: ");
+                    String productName = scanner.nextLine();
+                    store.addToCart(productName);
+                    break;
+                case 4:
+                    store.checkout();
+                    break;
+                case 0:
+                    System.out.println("Выход из приложения.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Неверный выбор.");
+            }
+        }
+    }
+}
