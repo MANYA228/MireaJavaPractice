1d4c99e6d243286d3479bfb744e3a99786173e66
MANYA <71900183+MANYA228@users.noreply.github.com>
2023-11-08 22:27:13 +0300

    practice 10!!!!
    Signed-off-by: MANYA <71900183+MANYA228@users.noreply.github.com>

---
+++ src/Practice10/Exercise15.java
@@ -0,0 +1,20 @@
+package Practice10;
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        int number = 12345;
+        printDigitsReverse(number);
+    }
+
+    public static void printDigitsReverse(int n) {
+        if (n < 10) {
+            // Если число меньше 10, выводим его и завершаем рекурсию
+            System.out.print(n + " ");
+        } else {
+            // Выводим последнюю цифру
+            System.out.print(n % 10 + " ");
+            // Рекурсивно вызываем функцию для числа без последней цифры
+            printDigitsReverse(n / 10);
+        }
+    }
+}
---
+++ src/Practice10/Exercise16.java
@@ -0,0 +1,35 @@
+package Practice10;
+
+import java.util.Scanner;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        int[] result = countMaxElements();
+        System.out.println("Наибольшее число: " + result[0]);
+        System.out.println("Количество элементов, равных максимуму: " + result[1]);
+    }
+
+    public static int[] countMaxElements() {
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == 0) {
+                break;
+            }
+
+            if (number > max) {
+                max = number;
+                count = 1;
+            } else if (number == max) {
+                count++;
+            }
+        }
+
+        int[] result = { max, count };
+        return result;
+    }
+}
+
